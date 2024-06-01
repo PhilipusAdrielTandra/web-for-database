89bccbf5ff97db5763394f75d06c8e71328ae040
CH1MP5T0N <philipusadriel@gmail.com>
2024-06-01 22:30:35 +0700

    changing ui

--- src/components/ChatbotData.jsx
+++ src/components/ChatbotData.jsx
@@ -41,41 +41,44 @@ const ContentList = () => {
           <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-2">Create tag</button>    
         </Link>  
       </div>
-      <ul>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {contents.map((content, index) => (
-          <li key={index} className="mb-2">
-            <div className="bg-gray-100 p-4 rounded-lg w-full">
-              <div className='flex justify-between'>
-                  <h2 className="text-left text-lg font-bold mb-2">{content.tag}</h2>
-                  <div>
-                    <Link to={`/edit/${content._id}`} className='mr-2'>Edit</Link>
-                    <a className='mr-2 hover:cursor-pointer' onClick={() => deleteTag(content._id)}>Delete</a>
-                  </div>
-              </div>
-              <div>
-                <h3 className="text-md font-semibold mb-1">Patterns:</h3>
-                <ul>
-                  {content.patterns.map((pattern, i) => (
-                    <li key={i} className="mb-1">
-                      {pattern}
-                    </li>
-                  ))}
-                </ul>
-              </div>
-              <div>
-                <h3 className="text-md font-semibold mb-1">Responses:</h3>
-                <ul>
-                  {content.responses.map((response, i) => (
-                    <li key={i} className="mb-1">
-                      {response}
-                    </li>
-                  ))}
-                </ul>
-              </div>
+          <div key={index} className="bg-gray-100 p-4 rounded-lg flex flex-col justify-between h-full">
+            <div className='flex justify-between'>
+                <h2 className="text-left text-lg font-bold mb-2 truncate overflow-hidden whitespace-nowrap text-ellipsis w-full">{content.tag}</h2>
             </div>
-          </li>
+            <div>
+              <h3 className="text-md font-semibold mb-1">Patterns:</h3>
+              <ul>
+                {content.patterns.slice(0,2).map((pattern, i) => (
+                  <li key={i} className="mb-1">
+                    {pattern}
+                    ...
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div>
+              <h3 className="text-md font-semibold mb-1">Responses:</h3>
+              <ul>
+                {content.responses.slice(0,2).map((response, i) => (
+                <li key={i} className="truncate w-full overflow-hidden whitespace-nowrap text-ellipsis mb-1">
+                  {response}
+                </li>
+                ))}
+              </ul>
+            </div>
+            <div className="mt-auto">
+              <Link to={`/tags/${content._id}`}>
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Details</button>
+                <Link to={`/edit/${content._id}`} className='mr-2'>Edit</Link>
+                  <a className='mr-2 hover:cursor-pointer' onClick={() => deleteTag(content._id)}>Delete</a>    
+              </Link>  
+            </div>
+          </div>
         ))}
-      </ul>
+        
+      </div>
     </div>
   );
 };
